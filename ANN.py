b4312a888ecfdd64b519604f0d70f0e4941fa3fb
NonOfUrBusiness <lucas.kempe@icloud.com>
2021-10-11 11:48:00 -0300

    new adversal neural net training...

---
+++ ANN.py
@@ -0,0 +1,15 @@
+import os
+import pprint
+import tempfile
+
+from typing import Dict, Text
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import tensorflow_recommenders as tfrs
+
+# Ratings data.
+ratings = tfds.load("movielens/100k-ratings", split="train")
+# Features of all the available movies.
+movies = tfds.load("movielens/100k-movies", split="train")
